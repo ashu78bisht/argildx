13d6bd5c893dba4dde48642f0cb77737bfddbd6c
ashu78bisht <37864867+ashu78bisht@users.noreply.github.com>
2021-05-16 22:39:03 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,33 @@ req.onreadystatechange = () => {
         console.log(responseData);
         document.getElementById("available").innerHTML = 'Available Offers ' + responseData.length;
         createList(responseData);
+        //jquery used only for pagination
+        pageSize = 6;
+        gridSize = Math.ceil(responseData.length / pageSize);
+
+        for (var i = 0; i < gridSize; i++) {
+            if (i == 0)
+                $("#pagin").append('<li><a class="current" href="javascript:void(0);">' + (i + 1) + '</a></li>');
+            else
+                $("#pagin").append('<li><a href="javascript:void(0);">' + (i + 1) + '</a></li>');
+        }
+
+        showPage = function(page) {
+            $(".content").hide();
+            $(".content").each(function(n) {
+                if (n >= gridSize * (page - 1) && n < gridSize * page)
+                    $(this).show();
+            });
+        }
+
+        showPage(1);
+
+        $("#pagin li a").click(function() {
+            $("#pagin li a").removeClass("current");
+            $(this).addClass("current");
+            showPage(parseInt($(this).text()))
+        });
+        //jquery used only for pagination
     }
 };
 
@@ -21,33 +48,7 @@ function createList(responseData) {
         document.getElementById("productListData").innerHTML += '<div class="col-lg-6 content"><div class="productList"><h3>' + item.Year + ' ' + item.Name + '</h3><figure><img src="' + item.Image + '" alt=""><figcaption>' + item.Rate + '/Day</figcaption></figure><ul><li>sports</li><li>' + item.Gearbox_type + '</li><li>' + item.Capacity + ' passengers</li><li>' + item.Fuel_type + '</li></ul></div></div>';
     });
 
-    //jquery used only for pagination
-    pageSize = 6;
-    gridSize = Math.ceil(responseData.length / pageSize);
 
-    for (var i = 0; i < gridSize; i++) {
-        if (i == 0)
-            $("#pagin").append('<li><a class="current" href="javascript:void(0);">' + (i + 1) + '</a></li>');
-        else
-            $("#pagin").append('<li><a href="javascript:void(0);">' + (i + 1) + '</a></li>');
-    }
-
-    showPage = function(page) {
-        $(".content").hide();
-        $(".content").each(function(n) {
-            if (n >= gridSize * (page - 1) && n < gridSize * page)
-                $(this).show();
-        });
-    }
-
-    showPage(1);
-
-    $("#pagin li a").click(function() {
-        $("#pagin li a").removeClass("current");
-        $(this).addClass("current");
-        showPage(parseInt($(this).text()))
-    });
-    //jquery used only for pagination
 }
 
 function onFilterChange() {
@@ -57,6 +58,7 @@ function onFilterChange() {
     if (gearFilter != '' || gearFilter != '') {
         var temp = responseData.filter(item => item.Fuel_type == fuelFilter || item.Gearbox_type == gearFilter);
         createList(temp);
+
     } else {
         createList(responseData);
     }
